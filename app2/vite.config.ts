668bd0e61059fb30f54d879724bcc0a77ddf81e7
Sergio Muro <sergiomuro@gmail.com>
2025-08-08 17:29:48 -0300

    Update

--- app2/vite.config.ts
+++ app2/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -10,7 +10,37 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-  ].filter(Boolean),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "NeuroLog App",
+        short_name: "NeuroLog",
+        description: "Aplicación NeuroLog",
+        theme_color: "#071b46",
+        background_color: "#071b46",
+        display: "fullscreen",
+        start_url: "/",
+        icons: [
+          {
+            src: "/assets/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/assets/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/assets/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
