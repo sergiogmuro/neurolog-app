1c9f65c2d07d11ac53b40ade0baa70e9856ddbdd
Sergio Muro <sergiomuro@gmail.com>
2025-08-11 15:20:47 -0300

    Update

--- app/src/pages/LoadingPage.tsx
+++ app/src/pages/LoadingPage.tsx
@@ -1,49 +1,22 @@
-import { useEffect, useState } from "react";
-import { Card } from "@/components/ui/card.tsx";
-import axiosClient from "@/api/axiosClient.ts";
+import {useEffect, useState} from "react";
+import {Card} from "@/components/ui/card.tsx";
 
 const LoadingPage = () => {
-  const [serverHealthy, setServerHealthy] = useState<boolean | null>(null);
-  const [checking, setChecking] = useState(true);
-
+  const [serverSlow, setServerSlow] = useState(false)
   useEffect(() => {
-    const controller = new AbortController();
-
-    const timeoutId = setTimeout(() => {
-      controller.abort();
+    setTimeout(() => {
+      setServerSlow(true)
     }, 5000);
-
-    axiosClient
-        .get("/healthz", { signal: controller.signal })
-        .then(response => {
-          if (response.status === 200) setServerHealthy(true);
-          else setServerHealthy(false);
-        })
-        .catch(error => {
-          if (error.name === "CanceledError") {
-            console.warn("Health check aborted due to timeout");
-          }
-          setServerHealthy(false);
-        })
-        .finally(() => {
-          setChecking(false);
-          clearTimeout(timeoutId);
-        });
-
-    return () => {
-      clearTimeout(timeoutId);
-      controller.abort();
-    };
   }, []);
 
   return (
-      <div className="cosmic-bg min-h-screen py-6 px-6 content-center" style={{ height: '100vh' }}>
+      <div className="cosmic-bg min-h-screen py-6 px-6 content-center" style={{height: '100vh'}}>
         <div className="text-center">
           <Card className="bg-transparent border-0 w-full text-center">
             <h2 className="text-5xl font-semibold text-foreground mb-8 text-left">
-              Cargando<span className={`text-orange-200`}>...</span>
+              Conectando al <span className={`text-orange-200`}>servidor...</span>
             </h2>
-            {!checking && serverHealthy === false && (
+            {!serverSlow === false && (
                 <p className="text-red-500 font-semibold mt-4">
                   El servidor está tardando o inactivo. Intentando reconectar...
                 </p>
