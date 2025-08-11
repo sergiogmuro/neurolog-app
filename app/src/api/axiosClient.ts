f3abb8cb4530999990b1cb1b8b8540878bfa3fe0
Sergio Muro <sergiomuro@gmail.com>
2025-08-11 13:06:59 -0300

    Update

--- app/src/api/axiosClient.ts
+++ app/src/api/axiosClient.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import * as process from "process";
 
-const baseURL = `${process.env.API_URL ?? import.meta.env.VITE_API_URL}:${process.env.API_PORT ?? import.meta.env.VITE_API_PORT}/api`;
-
+const baseURL = `${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}/api`;
+console.log(baseURL)
 const axiosClient = axios.create({
   baseURL,
   headers: {
